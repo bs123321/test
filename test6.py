38e0edbab3a9367a5149430bb90ec1d8941b86ca
tymf <101088445+bs123321@users.noreply.github.com>
2024-10-09 18:15:43 +0700

    09.10.24

--- test6.py
+++ test6.py
@@ -1,20 +1,20 @@
-string = input("").split(" ")
-
-number1 = float(string[0])
-
-znak = string[1]
-
-if znak == "m":
-    result = (number1 * 0.000621).__format__(".5f")
-    print("Mile", result)
-
-elif znak == "d":
-    result = (number1 * 39.3701).__format__(".5f")
-    print("Dyim",result)
-
-elif znak == "y":
-    result = (number1 * 1.09361).__format__(".5f")
-    print("Yards",result)
-
-else:
-    print("Неккоретные данные")
+# string = input("").split(" ")
+#
+# number1 = float(string[0])
+#
+# znak = string[1]
+#
+# if znak == "m":
+#     result = (number1 * 0.000621).__format__(".5f")
+#     print("Mile", result)
+#
+# elif znak == "d":
+#     result = (number1 * 39.3701).__format__(".5f")
+#     print("Dyim",result)
+#
+# elif znak == "y":
+#     result = (number1 * 1.09361).__format__(".5f")
+#     print("Yards",result)
+#
+# else:
+#     print("Неккоретные данные")
