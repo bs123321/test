561111ee2c368e82b1f2a0a3cdfa24c5ac871fbd
tymf <101088445+bs123321@users.noreply.github.com>
2024-11-04 20:25:04 +0700

    04.11

---
+++ test18.py
@@ -0,0 +1,45 @@
+
+# ж
+# print(' '*19, '_'*45)
+# i = 1
+# while i < 39:
+#     print(" " * 19, "|",
+#           "*" * i if i <= 20 else "*" *(39-i),
+#           " " * (39-i) if i <= 20 else " " * i, "|")
+#     i += 1
+# print(' '*19, '-'*45)
+#
+#
+# print(' '*19, '_'*46)
+# i = 1
+# while i < 40:
+#     if i <= 20:
+#         print(" " * 19, "|",
+#               "*" * i, " "*(40-i*2), "*"*i
+#               ,"|")
+#     else:
+#         print(" " * 19, "|",
+#               "*" * (40 - i), " " * ((i-20) * 2), "*" * (40 - i)
+#               , "|")
+#     i += 1
+# print(' '*19, '-'*46)
+
+# 13 dz
+
+# spisok = [-1, -2, -3, -5, 0, 6, 2, 74, 3]
+#
+# positive = 0
+# negative = 0
+# null = 0
+#
+# for x in spisok:
+#     if x < 0:
+#         negative += 1
+#     if x > 0:
+#         positive += 1
+#     if x == 0:
+#         null += 1
+#
+# print(positive, negative, null)
+# print(min(spisok))
+# print(min(spisok))
